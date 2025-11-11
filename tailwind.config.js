d79f256f670ffc9ec6c34f6b50158013102062bf
70chinemerem <chinemere2025@icloud.com>
2025-11-11 13:05:34 +0100

    my plugins

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}", // Where Tailwind will look for class names
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
